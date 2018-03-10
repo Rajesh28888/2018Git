0414adcd98d31d120b1a195ac44c3a1a1ac9e2e7
rajeshrc28888 <rajeshrc28@gmail.com>
2018-03-10 14:57:37 +0530

    added new file

---
+++ src/main/java/helloworld/Hellow.java
@@ -0,0 +1,8 @@
+    class Hellow{  
+        public static void main(String args[]){  
+         System.out.println("Hello Java 2018"); 
+
+         System.out.println("Hello Java 2017 and 2018!!!");
+ 
+        }  
+    }  
